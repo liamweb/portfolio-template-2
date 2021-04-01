b1592dc28570af946b8b0a9dbaaba2c64e922200
liamweb <60678514+liamweb@users.noreply.github.com>
2021-04-01 17:11:48 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("#mainNavbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
+
+//Smooth Scrolling
+$(".cf a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
